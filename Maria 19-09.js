8c6e42223eb71458da145f7e839559625e5f539e
R1beir0 <mariaterrar21@gmail.com>
2024-09-19 11:49:01 -0300

    Add files via upload

---
+++ Maria 19-09.js
@@ -0,0 +1,62 @@
+/*const nome = prompt("Qual é o seu nome?")
+const cor = prompt("Qual é a sua cor favorita?")
+let corFav = cor
+
+console.log("A cor favorita de " + nome + " é " + cor)*/
+
+
+/*const nome = prompt("Qual é o seu nome?")
+const cor = prompt("Qual é a sua cor favorita?")
+
+console.log(`A cor favorita de ${nome} é ${cor}`) */
+
+
+
+/*const frase = prompt ("Escreva uma frase!")
+const letraMaiuscula = frase.toUpperCase(frase)
+const linguaI = frase.replaceAll("o","i")
+
+console.log(`Frase: ${frase}`)
+console.log(`lingua do I: ${linguaI}`)
+console.log(`Frase maiuscula: ${letraMaiuscula}`)
+console.log(`Tamanho da frase: ${frase.length}`)*/
+
+
+/*const listaDog = ["Pitbull", "Pastor-alemão", "Buldogue", "Poodle", "Boxer"]
+
+const numero = prompt("Insira um número de 0 a 4")
+ 
+console.log(listaDog[numero])*/
+
+
+/*const numeros = [1, 2, 3, 4, 5, 6]
+console.log(numeros.length)
+
+numeros.push(7)
+console.log(numeros) 
+
+numeros.splice(3, 2)
+console.log(numeros)
+
+console.log(numeros.length)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
